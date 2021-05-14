eb9c70f894e1801e08341ecdf42e64653ca51227
miyu ono <r201901842lh@jindai.jp>
2021-05-15 03:55:11 +0900

    0515-1

--- script.js
+++
@@ -1,6 +0,0 @@
-const targetElement = document.querySelectorAll(".animationTarget");
-targetElement.forEach(element => {
-    const getElementDistance = targetElement[i].
-    getBoundingClientRect().top
-    console.log(getElementDistance);
-}
